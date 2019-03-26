2646bfa170c97762f2b7bfb64266667ef1cbfd36
Jamie Ireland <jamieireland88@outlook.com>
2019-03-26 21:49:23 +0000

    JS/CSS

--- js/actions.js
+++ js/actions.js
@@ -1,1 +1,3 @@
-console.log("actions loaded");
+$('#container').append($('<div/>', {'id': 'colourBox'}));
+TweenMax.set($('#colourBox'), {width: '400%', height: '100%', background: 'linear-gradient(to left, red, blue, red)'})
+TweenMax.fromTo($('#colourBox'), 5, {x: '0%'}, {x: '-50%', repeat: -1});
