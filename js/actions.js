bb4093cb80b38960388648fc147016320f1f6c8f
Jamie Ireland <jamieireland88@outlook.com>
2019-03-31 23:10:26 +0100

    New Domain Name

--- js/actions.js
+++ js/actions.js
@@ -1,3 +1,6 @@
 $('#container').append($('<div/>', {'id': 'colourBox'}));
-TweenMax.set($('#colourBox'), {width: '400%', height: '100%', background: 'linear-gradient(to left, red, blue, red)'})
-TweenMax.fromTo($('#colourBox'), 5, {x: '0%'}, {x: '-50%', repeat: -1});
+TweenMax.set($('#colourBox'), {width: '100%', height: '100%'})
+var tl = new TimelineMax({timeScale: 10, repeat: -1});
+tl.fromTo($('#colourBox'), 5, {backgroundColor: '#cc0000'}, {backgroundColor: '#0000cc'});
+tl.fromTo($('#colourBox'), 5, {backgroundColor: '#0000cc'}, {backgroundColor: '#cccc00'});
+tl.fromTo($('#colourBox'), 5, {backgroundColor: '#cccc00'}, {backgroundColor: '#cc0000'});
